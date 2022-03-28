b7f91e13f87026dbc05c0c28add0746cfd7a6f80
nityanandakaruturi <102410308+Nitya7797@users.noreply.github.com>
2022-03-28 18:39:58 +0530

    Create HighestNumber.java

---
+++ HighestNumber.java
@@ -0,0 +1,37 @@
+import java.util.scanner;
+
+public class HighestNumber {
+     
+//Add main method
+
+ public static void main(String[] args)
+   {
+      int a,b,c;
+
+//Declare the three variables
+
+//Use the scanner class to provide input at execution time
+
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter the first number:");
+      a=sc.nextInt();
+      System.out.println("Enter the Second number:");
+      b=sc.nextInt();
+      System.out.println("Enter the Third number:");
+      c=sc.nextInt();
+      if(a>b && a>c)
+         System.out.println(""+a+" is highest number");
+      else if(b>c)
+         System.out.println(""+b+" is highest number);
+      else
+         System.out.println(""+c+" is highest number");
+   }
+//check which number is highest using if else condition
+//Print the highest of three numbers
+ 
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
